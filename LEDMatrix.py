8eedf582870fdf7dbf5df92541448cdcefa769c6
Jean-Marc Boullianne <jboullianne@gmail.com>
2016-06-26 01:40:51 +0000

    Added LEDSquare Object

--- LEDMatrix.py
+++ LEDMatrix.py
@@ -39,3 +39,21 @@ class LEDMatrix:
 					self.setPixel(y,x, (0,0,0))
 				else:
 					self.setPixel(y, x, (rgb[0], rgb[1], rgb[2]))
+
+class LEDSquare:
+	def __init__(self, x1, y1, x2, y2, rgb, matrix):
+		self.x1 = x1
+		self.x2 = x2
+		self.y1 = y1
+		self.y2 = y2
+		self.rgb = rgb
+		self.matrix = matrix
+		self.draw()
+
+	def draw(self):
+		self.matrix.fillRect(self.x1, self.y1, self.x2, self.y2, self.rgb)
+
+	def moveX(self, x):
+		self.x1 += x
+		self.x2 += x
+		self.draw()
--- test.py
+++ test.py
@@ -2,14 +2,29 @@
 # Running on a 2 32x32 LED Matrix boards
 import time
 from LEDMatrix import LEDMatrix
+from LEDMatrix import LEDSquare
 
 def main():
 	print "Starting LEDMatrix Test"
 
-	#Constructing LEDMatrix
+	# Constructing LEDMatrix
 	matrix = LEDMatrix(32, 2)
+
+	# Set Background Green
 	matrix.setFill(0x00FF00)
+	time.sleep(1)
+
+	matrix.fillRect(0,0,15,15, (0,0,255))
+	time.sleep(1)
+
+	square = LEDSquare(15,0,30,15, (255,0,0), matrix)
+	time.sleep(1)
+
+	# Moves square's position 3 pixels to the right
+	square.moveX(3)
 	time.sleep(2)
+
+	# Clears the entire matrix LED board
 	matrix.clear()
 
 if __name__ == "__main__":
