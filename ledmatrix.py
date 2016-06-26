b59adc8d72d6aec68b962e24527a82a1d03d19e7
Jean-Marc Boullianne <jboullianne@gmail.com>
2016-06-26 00:11:49 +0000

    Initial Commit

---
+++ ledmatrix.py
@@ -0,0 +1,41 @@
+from rgbmatrix import Adafruit_RGBmatrix
+import Image
+import ImageDraw
+
+# Ease of Use API For Matrix
+class MatrixDriver:
+	
+	def __init__(self, height, chains):
+		self.matrix = Adafruit_RGBmatrix(height, chains)
+		self.MAX_Y = height
+		self.MAX_X = height * chains
+
+	def setFill(self, hex_color):
+		self.matrix.Fill(hex_color)
+
+	def clear(self):
+		self.matrix.Clear()
+
+	def setPixel(self, x, y, rgb):
+		self.matrix.SetPixel(x, y, *rgb)
+
+	def fillRect(self, x1, y1, x2, y2, rgb):
+		for x in range(x1, x2):
+			for y in range(y1, y2):
+				self.setPixel(x, y, rgb)
+
+	def drawPointList(self, plist, rgb):
+		for point in plist:
+			self.setPixel(point[0],point[1], rgb)
+
+	def drawImage(self, file_name):
+		image = Image.open(file_name)
+		raw = image.load()
+
+		for x in range(0,30):
+			for y in range(0,63):
+				rgb = raw[y, x]
+				if rgb == (0,0,0): #If you wanted to make the background another color
+					self.setPixel(y,x, (0,0,0))
+				else:
+					self.setPixel(y, x, (rgb[0], rgb[1], rgb[2]))
